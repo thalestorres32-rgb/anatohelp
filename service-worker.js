ed5cb1180830ef78e5761d315ffd861624d44822
thalestorres32-rgb <thalestorres32@gmail.com>
2026-03-16 21:13:55 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,24 @@
 const CACHE_NAME = "anatohelp-v1";
 
+// Liste todos os arquivos importantes aqui (CSS, JS, Imagens de anatomia)
 const urlsToCache = [
   "./",
   "./index.html",
   "./menu.html",
-  "./style.css"
+  "./style.css",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting(); // Força a atualização para a nova versão
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim()); // Assume o controle da página imediatamente
 });
 
 self.addEventListener("fetch", event => {
