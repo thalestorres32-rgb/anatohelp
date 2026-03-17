6d253b0a5a187f6a265c6100da1e28f86600c832
thalestorres32-rgb <thalestorres32@gmail.com>
2026-03-16 21:32:51 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,19 @@
-const CACHE_NAME = "anatohelp-v3";
-const assets = [
-  "./",
-  "./index.html",
-  "./menu.html",
-  "./style.css",
-  "./manifest.json"
-];
+const CACHE_NAME = "anatohelp-v4";
+const assets = ["./", "./index.html", "./style.css", "./manifest.json", "./icon-192.png"];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return Promise.all(
-        assets.map(asset => {
-          return cache.add(asset).catch(err => console.log("Pulando arquivo opcional: " + asset));
-        })
+        assets.map((path) => cache.add(path).catch(() => console.log("Erro no cache de: " + path)))
       );
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
