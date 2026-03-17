38a3b7cf44540ef64e05709236ec88fd9e83046d
thalestorres32-rgb <thalestorres32@gmail.com>
2026-03-16 21:53:54 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,36 @@
-const CACHE_NAME = "anatohelp-v6-icons";
-const assets = [
+const CACHE_NAME = "anatohelp-v1";
+
+const urlsToCache = [
   "./",
   "./index.html",
+  "./menu.html",
   "./style.css",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
+self.addEventListener("install", event => {
+  self.skipWaiting(); 
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      // Adiciona um por um para não travar se algum falhar
-      return Promise.all(
-        assets.map((path) => cache.add(path).catch(() => console.log("Aguardando arquivo: " + path)))
-      );
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return Promise.all(
+          urlsToCache.map(url => {
+            return cache.add(url).catch(err => console.log("Erro ao cachear:", url));
+          })
+        );
+      })
   );
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(clients.claim());
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
